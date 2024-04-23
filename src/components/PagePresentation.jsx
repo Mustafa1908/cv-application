5c5657bca10bfe8f48f999bfafebbcff69ee25ea
Mustafa Özkara <mustafaozkara@outlook.fr>
2024-04-23 13:25:31 +0200

    Add presentation text of the page and header

---
+++ src/components/PagePresentation.jsx
@@ -0,0 +1,15 @@
+import "../styles/PagePresentation.css";
+
+export default function PagePresentation() {
+  return (
+    <>
+      <header>
+        <h1 className="pagePresentationHeader">cv generator</h1>
+        <p className="pagePresentationText">
+          Create your CV by filling out the forms below! Your CV will be updated
+          in the preview.
+        </p>
+      </header>
+    </>
+  );
+}
