c60b0e53c8c04ce493c6a941a1ef9bdf8cebff17
Mustafa Özkara <mustafaozkara@outlook.fr>
2024-04-24 15:10:46 +0200

    Change file Name to Header, and change header1 className to header

---
+++ src/components/Header.jsx
@@ -0,0 +1,15 @@
+import "../styles/Header.css";
+
+export default function Header() {
+  return (
+    <>
+      <header>
+        <h1 className="header">cv generator</h1>
+        <p className="pagePresentationText">
+          Create your CV by filling out the forms below! Your CV will be updated
+          in the preview.
+        </p>
+      </header>
+    </>
+  );
+}
