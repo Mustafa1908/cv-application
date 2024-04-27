03b8bee7b021fd7d0f4b5750268f1e4cd0224751
Mustafa Özkara <mustafaozkara@outlook.fr>
2024-04-27 15:49:12 +0200

    Import each section of the page and delete Form and Cv files

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,63 @@
-import PagePresentation from "./components/PagePresentation";
+import Header from "./components/Header";
+import BasicInfo from "./components/BasicInfo";
+import ContactInfo from "./components/ContactInfo";
+import EducationBackground from "./components/EducationBackground";
+import WorkExperience from "./components/WorkExperience";
+import TechnicalSkills from "./components/TechnicalSkills";
+import CvBasicInfo from "./components/CvBasicInfo";
+import CvEducationBackground from "./components/CvEducationBackground";
+import CvWorkExperience from "./components/CvWorkExperience";
+import CvContact from "./components/CvContact";
+import CvSkills from "./components/CvSkills";
 import "./styles/styles.css";
-import Form from "./components/Form";
+import "./styles/Form.css";
+import "./styles/Cv.css";
 
 function App() {
   return (
     <>
       <div className="pageContainer">
         <div className="leftSidePage">
-          <PagePresentation />
-          <Form />
+          <Header />
+          <main>
+            <form>
+              <BasicInfo />
+              <ContactInfo />
+            </form>
+            <form>
+              <EducationBackground />
+            </form>
+            <form>
+              <WorkExperience />
+            </form>
+            <form>
+              <TechnicalSkills />
+            </form>
+          </main>
         </div>
-        <div className="rightSidePage"></div>
+        <div className="rightSidePage">
+          <section className="cvContainer">
+            <div className="leftSideContainer">
+              <section className="basicInfoContainer">
+                <CvBasicInfo />
+              </section>
+              <section>
+                <CvEducationBackground />
+              </section>
+              <section>
+                <CvWorkExperience />
+              </section>
+            </div>
+            <div className="rightSideContainer">
+              <section className="contactInfoContainer">
+                <CvContact />
+              </section>
+              <section>
+                <CvSkills />
+              </section>
+            </div>
+          </section>
+        </div>
       </div>
     </>
   );
