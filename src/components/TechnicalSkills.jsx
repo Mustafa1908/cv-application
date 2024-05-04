f3e39de6d74319a5ec76710064644f6c8c562840
Mustafa Özkara <mustafaozkara@outlook.fr>
2024-05-04 15:43:13 +0200

    Add skill on cv and container to edit and delete skill item when submitting skill

--- src/components/TechnicalSkills.jsx
+++ src/components/TechnicalSkills.jsx
@@ -1,3 +1,5 @@
+import { useState } from "react";
+
 function Input({ inputText, inputType, inputId, labelClass, placeHolderText }) {
   return (
     <>
@@ -9,6 +11,7 @@ function Input({ inputText, inputType, inputId, labelClass, placeHolderText }) {
         name={inputId}
         id={inputId}
         placeholder={placeHolderText}
+        required
       />
     </>
   );
@@ -23,21 +26,95 @@ function SecondHeader({ headerName, headerClass }) {
   );
 }
 
-function InputSubmit({ buttonText, className }) {
+function SimpleListItem({ listItemText, listItemClass }) {
+  return <li className={listItemClass}>{listItemText}</li>;
+}
+
+function InputSubmit({ buttonText, className, onChangeFunction }) {
   return (
     <div className="submitButtonContainer">
-      <input type="submit" value={buttonText} class={className} />
+      <input
+        type="submit"
+        value={buttonText}
+        class={className}
+        onClick={onChangeFunction}
+      />
+    </div>
+  );
+}
+
+function EditDeleteItem({
+  itemContainerClass,
+  itemText,
+  itemTextClass,
+  iconsContainerClass,
+  editiconClass,
+  deleteIconClass,
+}) {
+  return (
+    <div className={itemContainerClass}>
+      <p className={itemTextClass}>{itemText}</p>
+      <div className={iconsContainerClass}>
+        <span class={"material-symbols-outlined " + editiconClass}>delete</span>
+        <span class={"material-symbols-outlined " + deleteIconClass}>edit</span>
+      </div>
     </div>
   );
 }
 
 export default function TechnicalSkills() {
+  const [inputValue, setInputValue] = useState([]);
+
+  if (inputValue.length >= 1) {
+    let skillDeleteEditContainer = document.querySelector(
+      ".skillDeleteEditContainer"
+    );
+    let skillContainer = document.querySelector(".skillContainer");
+
+    skillContainer.innerHTML = "";
+    skillDeleteEditContainer.innerHTML = "";
+
+    for (let i = 0; i < inputValue.length; i++) {
+      let newSkillItem = document.createElement("li");
+      let editDeleteItem = document.createElement("div");
+
+      editDeleteItem.className = "skillModifyContainer";
+      editDeleteItem.innerHTML = `<p class="skillModifyText">${inputValue[i]}</p><div class="editDeleteContainer"><span class="material-symbols-outlined skillModifyIcon">delete</span><span class="material-symbols-outlined skillDeleteIcon">edit</span></div>`;
+
+      newSkillItem.className = "skillText";
+      newSkillItem.innerText = inputValue[i];
+
+      skillDeleteEditContainer.appendChild(editDeleteItem);
+      skillContainer.appendChild(newSkillItem);
+    }
+  }
+
+  function handleInputValuechange(e) {
+    e.preventDefault();
+
+    let skillValue = document.querySelector("#userSkill");
+
+    let skillValueArray = [skillValue.value];
+    setInputValue(inputValue.concat(skillValueArray));
+    skillValue.value = "";
+  }
+
   return (
     <>
       <SecondHeader
         headerName={"Technical Skills"}
         headerClass={"formHeader"}
       />
+      <div className="skillDeleteEditContainer">
+        <EditDeleteItem
+          itemContainerClass={"skillModifyContainer"}
+          itemText={"Html"}
+          itemTextClass={"skillModifyText"}
+          iconsContainerClass={"editDeleteContainer"}
+          editiconClass={"skillModifyIcon"}
+          deleteIconClass={"skillDeleteIcon"}
+        />
+      </div>
       <Input
         inputText={"Add a skill"}
         inputType={"text"}
@@ -47,7 +124,8 @@ export default function TechnicalSkills() {
       />
       <InputSubmit
         buttonText={"Submit technical skills"}
-        className={"submitInput"}
+        className={"submitInput submitSkill"}
+        onChangeFunction={handleInputValuechange}
       />
     </>
   );
