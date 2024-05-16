78811b3d7241f032424d8dc7438ebac17708a412
Mustafa Özkara <mustafaozkara@outlook.fr>
2024-05-16 15:44:37 +0200

    Remove import of useState, add button with function printCvPage to print the cv on click

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,8 +1,17 @@
 export default function Header() {
+  function printCvPage() {
+    window.print();
+  }
+
   return (
     <>
       <header>
-        <h1 className="header">cv generator</h1>
+        <div className="headerPrintContainer">
+          <h1 className="header">cv generator</h1>
+          <div onClick={printCvPage}>
+            <span class="material-symbols-outlined printCvIcon">print</span>
+          </div>
+        </div>
         <p className="pagePresentationText">
           Create your CV by filling out the forms below! Your CV will be updated
           in the preview.
